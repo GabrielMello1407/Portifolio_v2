35621497476f61bee81e0b6438d5591369632796
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2023-10-19 16:38:05 -0300

    Navbar createCheck
    navbarCreating

--- portifolio/src/app/components/HeroSection.jsx
+++ portifolio/src/app/components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className='text-white mb-4 text-4xl sm:text-5xl foi criada a seção hero do portifólio , e devidamente estilizadalg:text-6xl font-extrabold'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-red-600 ">Hello World! I'm {''}</span>
             <br />
             <TypeAnimation
@@ -21,27 +21,25 @@ const HeroSection = () => {
                 1000, // wait 1s before replacing "Mice" with "Hamsters"
                 'Front-end Developer',
                 1000,
-                'UI/UX Designer',
-                1000
               ]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h1>
-          <p className='text-[#ADB7BE] text-base sm:text-lg text-lg mb-6 lf:text-xl'>Front-End Developer</p>
+          <p className='text-[#ADB7BE] text-base sm:text-lg text-lg mb-6 lf:text-xl'>🚀💻 Me chamo Gabriel Mello, sou formado em Análise e Desenvolvimento de Sistemas, estou sempre de olho em novas formas de aprimorar minhas habilidades, em um ambiente que é uma montanha-russa de desafios, pois o front-end está sempre mudando. Estou empolgado para superar as expectativas, abraçar novos desafios tecnológicos e continuar crescendo como um ninja do front-end! 🚀💻</p>
           <div>
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-yellow-500 via-red-500 to-purple-500 hover:bg-slate-200 text-white'>Sobre Mim</button>
             <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-yellow-500 via-red-500 to-purple-500 hover:bg-slate-800 text-white  mt-3'>
-              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span>
+              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download Currículo</span>
             </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src='/images/logo.svg'
-              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              src='/images/eu.png'
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full'
               alt='hero image'
               width={300}
               height={300}
