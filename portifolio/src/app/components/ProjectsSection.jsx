06520b4be70fe50db90a1e022cbf2121632dc2b4
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2023-10-24 15:45:09 -0300

    feat: target blank add

--- portifolio/src/app/components/ProjectsSection.jsx
+++ portifolio/src/app/components/ProjectsSection.jsx
@@ -12,7 +12,7 @@ const projectsData = [
     description: "Criação de um blog utilizando html e css com responsividade",
     image: "/images/projects/Blog_semantico.jpeg",
     tag: ["All", "Básico", "HTML", "CSS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Blog-Sem-ntico",
     previewUrl: "/"
   },
   {
@@ -21,16 +21,16 @@ const projectsData = [
     description: "Criação de um blog utilizando React",
     image: "/images/projects/blog_react.jpeg",
     tag: ["All", "HTML", "CSS", "React"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Projeto-Blog-react",
     previewUrl: "/"
   },
   {
     id: 3,
     title: "Projeto Calculadora IMC",
-    description: "Criação de uma Calculadora de IMC",
+    description: "Criação de uma Calculadora de IMC em React",
     image: "/images/projects/calc_imc.jpeg",
-    tag: ["All", "Básico", "HTML", "CSS", "JavaScript"],
-    gitUrl: "/",
+    tag: ["All", "Básico", "HTML", "CSS", "JavaScript", "React"],
+    gitUrl: "https://github.com/GabrielMello1407/Calculadora-de-IMC-React",
     previewUrl: "/"
   },
   {
@@ -39,7 +39,7 @@ const projectsData = [
     description: "Criação clone da página do Iphone 13 para treinar HTML,CSS e responsividade",
     image: "/images/projects/clone_iphone13.jpeg",
     tag: ["All", "Básico", "HTML", "CSS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Clone-iphone-page",
     previewUrl: "/"
   },
   {
@@ -48,7 +48,7 @@ const projectsData = [
     description: "Criação de um mini formulário com vários passos",
     image: "/images/projects/form_multistep.jpeg",
     tag: ["All", "Básico", "HTML", "CSS", "JavaScript"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Form-MultiStep-React",
     previewUrl: "/"
   },
   {
@@ -57,7 +57,7 @@ const projectsData = [
     description: "Criação de um gerador de QRCode realizando fetch com API para realizar a geração dos QRCode baseado no que o usuário digita",
     image: "/images/projects/geradorQRCode.jpeg",
     tag: ["All", "HTML", "CSS", "JavaScript", "API"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Gerador-de-QR-Code",
     previewUrl: "/"
   },
   {
@@ -66,7 +66,7 @@ const projectsData = [
     description: "Criação de um sistema de buscar perfil com base o nome de usuário do github utilizando a api do github",
     image: "/images/projects/github_finder.jpeg",
     tag: ["All", "HTML", "CSS", "React", "API", "TS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Project-github-Finder",
     previewUrl: "/"
   },
   {
@@ -75,7 +75,7 @@ const projectsData = [
     description: "Criação clone da página de login do instagram com responsividade",
     image: "/images/projects/instagram_clone.jpeg",
     tag: ["All", "Básico", "HTML", "CSS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Page-login-instagram-clone",
     previewUrl: "/"
   },
   {
@@ -84,7 +84,7 @@ const projectsData = [
     description: "Criação de uma página básica utilizando o método de lazy load para carregamento de imagens",
     image: "/images/projects/lazy_load.jpeg",
     tag: ["All", "Básico", "HTML", "CSS", "JavaScript"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Projeto-lazy-load",
     previewUrl: "/"
   },
   {
@@ -93,7 +93,7 @@ const projectsData = [
     description: "Criação de uma landing page onde é possivel registrar memórias com descrição podendo upar imagens e adicionar tags",
     image: "/images/projects/memories.jpeg",
     tag: ["All", "HTML", "CSS", "React", "Node", "MongoDB", "Axios"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Projeto-Memories",
     previewUrl: "/"
   },
   {
@@ -102,7 +102,7 @@ const projectsData = [
     description: "Criação um projeto de criação de festas",
     image: "/images/projects/partytime.jpeg",
     tag: ["All", "HTML", "CSS", "React", "Node", "MongoDB", "Axios"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Projeto-Party-Time",
     previewUrl: "/"
   },
   {
@@ -111,7 +111,7 @@ const projectsData = [
     description: "Criação do meu primeiro portifólio responsivo",
     image: "/images/projects/portifolio_antigo.jpeg",
     tag: ["All", "Básico", "HTML", "CSS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Portifolio-novo",
     previewUrl: "/"
   },
   {
@@ -120,7 +120,7 @@ const projectsData = [
     description: "Criação de uma lista de tarefas",
     image: "/images/projects/projetoTodoList.jpeg",
     tag: ["All", "Básico", "HTML", "CSS"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/Projeto-todo-avancado",
     previewUrl: "/"
   },
   {
@@ -129,7 +129,7 @@ const projectsData = [
     description: "Criação de um Quiz utilizando react",
     image: "/images/projects/quizReact.jpeg",
     tag: ["All", "Básico", "HTML", "CSS", "React"],
-    gitUrl: "/",
+    gitUrl: "https://github.com/GabrielMello1407/ProjetoQuiz_React",
     previewUrl: "/"
   },
 ]
@@ -155,7 +155,7 @@ const ProjectsSection = () => {
   return (
     <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+        Meus Projetos
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
